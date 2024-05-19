a822d9e78d54d2a6872f551bb3a55888257e5aa6
YanRodrigues <133587638+Yan-MR@users.noreply.github.com>
2024-05-18 21:22:41 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cronometro from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Cronometro />
+  </React.StrictMode>,
+)
